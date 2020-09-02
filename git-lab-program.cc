458c2e924f3d1d0f7122a74e2d2deda2af222ccc
Andrew Yang <70236501+ayang55@users.noreply.github.com>
2020-09-01 20:42:36 -0400

    Coding hurts my soul

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Andrew Yang
+ *        Date: 9/1/2020
+ * Description: Coding is hard
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
